e919dc5054fdadc330476d1d23b1bbcee5d65edb
Ocean <ocean1793@163.com>
2019-06-25 00:16:18 +0800

    工厂设计模式
    抽象工厂-驾驶多种交通工具

--- design-pattern/src/main/java/ocean/factory/abstractfactory/AbstractFactory.java
+++ design-pattern/src/main/java/ocean/factory/abstractfactory/AbstractFactory.java
@@ -1,9 +1,18 @@
 package ocean.factory.abstractfactory;
 
 /**
+ * 1、形容词用（可以干什么干什么）接口，名称用抽象类
+ * 从语义上区分，到底是用抽象类，还是接口
+ * <p>
+ * 2.工厂方法和抽象工厂的对比
+ * 工厂方法便于产品上的扩展，产品维度上
+ * 抽象工厂产品一族的扩展，产品一族上扩展上
+ * <p>
+ * 针对2的问题，更好的解决方法使用Spring IoC的bean工厂
+ *
  * @author yancy
  * @date 2019/6/24
  */
 public class AbstractFactory {
-    abstract Food
+//    abstract Food
 }
