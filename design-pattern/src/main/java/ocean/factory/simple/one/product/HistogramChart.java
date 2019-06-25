fffb7b56e89d56f62df63abba611c86b31272ac7
Ocean <ocean1793@163.com>
2019-06-25 23:13:17 +0800

    简单工厂模式

--- design-pattern/src/main/java/ocean/factory/simple/one/product/HistogramChart.java
+++ design-pattern/src/main/java/ocean/factory/simple/one/product/HistogramChart.java
@@ -1,6 +1,10 @@
 package ocean.factory.simple.one.product;
 
 public class HistogramChart implements Chart {
+    public HistogramChart() {
+        System.out.println("创建柱状图");
+    }
+
     @Override
     public void display() {
         System.out.println("显示柱状图");
--- design-pattern/src/main/java/ocean/factory/simple/one/product/LineChart.java
+++ design-pattern/src/main/java/ocean/factory/simple/one/product/LineChart.java
@@ -1,6 +1,10 @@
 package ocean.factory.simple.one.product;
 
 public class LineChart implements Chart{
+    public LineChart() {
+        System.out.println("创建饼状图");
+    }
+
     @Override
     public void display() {
         System.out.println("显示饼状图");
