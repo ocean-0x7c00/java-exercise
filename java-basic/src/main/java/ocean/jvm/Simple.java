16da2ba1ae7eb3f6b6fc5dddc22369d66d45ac5d
Ocean <ocean1793@163.com>
2019-07-29 00:15:17 +0800

    java class structure

---
+++ java-basic/src/main/java/ocean/jvm/Simple.java
@@ -0,0 +1,9 @@
+package ocean.jvm;
+
+/**
+ * @author yancy
+ * @date 2019/7/28
+ */
+public class Simple {
+    private final static String name = "hello";
+}
---
+++ java-basic/src/main/java/ocean/jvm/TestClass.java
@@ -0,0 +1,13 @@
+package ocean.jvm;
+
+/**
+ * @author yancy
+ * @date 2019/7/29
+ */
+public class TestClass {
+    private int m;
+
+    public int inc() {
+        return m + 1;
+    }
+}
