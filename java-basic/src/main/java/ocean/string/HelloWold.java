f8c61a9b296456f502739d0debe2b4c3b396354e
Ocean <ocean1793@163.com>
2019-08-05 22:38:14 +0800

    2.可执行类中可以有零个或多个属性，但必须有一个main方法

--- java-basic/src/main/java/ocean/string/HelloWold.java
+++ java-basic/src/main/java/ocean/string/HelloWold.java
@@ -3,11 +3,21 @@ package ocean.string;
 
 /**
  * 一个不包含任何属性和方法的类是可以通过编译的，所以类是Java最小的可编译单元
- *
+ * 类中可以包含属性和方法。方法由若干个语句组成。语句又是由表达式构成
+ * 可执行的Java程序必须要包含程序的入口-main方法
  *
  * @author yancy
  * @date 2019/8/5
  */
 public class HelloWold {
+    int field = 0;
+
+    public void method() {
+    }
+
+    public static void main(String[] args) {
+
+    }
+
 }
 
