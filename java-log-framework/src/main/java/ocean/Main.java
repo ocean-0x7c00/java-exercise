c65cb41886cb9bc51753eeb789ae5a4ba1635008
Lee <>
2019-08-05 15:42:55 +0800

    java日志系统配置练习

--- java-log-framework/src/main/java/ocean/Main.java
+++ java-log-framework/src/main/java/ocean/Main.java
@@ -5,13 +5,25 @@ package ocean;
  * <p>
  * [Java 日志系统梳理](http://patchouli-know.com/2017/04/08/java-log/)
  * <p>
- * Apache log4j  日志系统
+ * Apache log4j
  * --> JUL(Java.util.logging,java 1.4引入，影响：导致java日志体系混乱)  日志系统
- * --> Apache commons-logging (简称JCL，日志框架，它不是一个日志打印系统，通用日志接口定义，起到适配和连接的作用,jul_02,log4j是具体的实现)
- * --> Slf4j(通用日志接口，日志框架，logback是官方的实现日志系统；slf4j为了解决jcl动态加载实现类引发的问题)
- * --> Log4j2 同时也支持slf4j和jcl  日志系统
+ * --> Apache commons-logging (简称JCL，它不是一个日志打印系统，通用日志接口定义，起到适配和连接的作用,jul_02,log4j是具体的实现)
+ * --> Slf4j(通用日志接口，logback是官方的实现日志系统；slf4j为了解决jcl动态加载实现类引发的问题)
+ * --> Log4j2 同时也支持slf4j和jcl
+ * <p>
  * 2.Java日志框架有哪些？
- * 3.如何配置日志框架
+ * Apache commons-logging 和 Slf4j
+ * 日志框架：只是一种日志接口，不负责具体的日志输出实现，而是通过配置来灵活的选择具体的日志系统。例如 jcl、slf4j 就是日志框架，它们只提供了接口，没有提供具体的日志系统实现；
+ * 日志系统：真正的日志实现，如 log4j、jul、logback 与 log4j2 都是具体的日志系统实现。
+ * <p>
+ * 日志系统有
+ * log4j、JUL、logback和log4j2
+ * <p>
+ * 3.如何配置日志系统
+ * 配置Logger
+ * 配置Appender
+ * 配置Layout
+ * <p>
  * 4.日志框架的工作原理
  *
  * @author ocean
--- java-log-framework/src/main/java/ocean/log4j_01/Client.java
+++ java-log-framework/src/main/java/ocean/log4j_01/Client.java
@@ -2,15 +2,37 @@ package ocean.log4j_01;
 
 
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 /**
+ * 使用日志系统 log4j
+ * <p>
+ * 1.只要log4j的配置文件名为log4j.properties且该配置文件在类根路径(resources)下，
+ * 则程序可自动加载该配置文件，不用写任何代码，log4j就会初试化
+ *
+ * 2.可以通过PropertyConfigurator.configure("");
+ * 语句来加载自定义命名的配置文件（包括log4j.properties），但是得明确指定配置文件的放置目录
+ *
+ *
  * @author ocean
  * @date 2019/8/5
  */
 public class Client {
-    private static Logger logger = Logger.getLogger(Client.class);
+    /**
+     * 得到记录器
+     */
+    private static Logger log = Logger.getLogger(Client.class);
 
     public static void main(String[] args) {
-        logger.info("hello");
+
+        //在src下使用全路径
+        //PropertyConfigurator.configure("/Users/ocean/Documents/java-exercise/java-log-framework/src/main/java/ocean/log4j_01/log4j.properties");
+
+//        PropertyConfigurator.configure("../conf/log4j.properties");
+
+        log.info("info");
+        log.error("error");
+        log.warn("warn");
+        log.debug("debug");
     }
 }
