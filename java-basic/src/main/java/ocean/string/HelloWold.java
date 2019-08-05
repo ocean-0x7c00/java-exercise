ee5f3d22de95d2dba51fde483773b8bfacf571a8
Ocean <ocean1793@163.com>
2019-08-06 00:17:59 +0800

    Java程序结构学习

--- java-basic/src/main/java/ocean/string/HelloWold.java
+++ java-basic/src/main/java/ocean/string/HelloWold.java
@@ -2,9 +2,20 @@ package ocean.string;
 
 
 /**
+ * 一.Java程序的基本结构-由类、属性和方法构成的
  * 一个不包含任何属性和方法的类是可以通过编译的，所以类是Java最小的可编译单元
- * 类中可以包含属性和方法。方法由若干个语句组成。语句又是由表达式构成
- * 可执行的Java程序必须要包含程序的入口-main方法
+ * 类中可以包含属性和方法。方法由若干个语句组成，语句又是由表达式或变量构成，变量和运算符组成基本的表达式或语句
+ * 可执行的Java程序必须要包含程序的入口- main方法```public static void main(String[] args)```
+ * <p>
+ * 二.标识符
+ * 使用表示符来表示类、对象、变量的名字
+ * 标识符的规则:Java标识符可以由除了数字开头的26个英文大小字符、数字(0-9)、下划线(_)和美元符号($)自由组合而成
+ * <p>
+ * 三.变量和常量
+ * 1.变量
+ * 变量在使用前要经历声明和初始化
+ * Java支持两类数据类型
+ * a.基本
  *
  * @author yancy
  * @date 2019/8/5
@@ -12,7 +23,7 @@ package ocean.string;
 public class HelloWold {
     int field = 0;
 
-    public void method() {
+    void method() {
     }
 
     public static void main(String[] args) {
